685fe86958dd9e7eceea660d8dd1bb3f42bc1724
Laadjal Faouzi <faouzi.laadjal99@gmail.com>
2022-09-11 14:20:08 +0100

    modified skills section

--- src/Components/Skills/Styles/SkillsFlex.styled.js
+++ src/Components/Skills/Styles/SkillsFlex.styled.js
@@ -2,9 +2,14 @@ import styled from "styled-components";
 export const SkillsFlex = styled.div`
   padding: 1em;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   @media screen and (max-width: 68em) {
     flex-direction: column;
     gap: 4em;
+    background: none;
   }
+  background: url("images/skills.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 500px auto;
 `;
