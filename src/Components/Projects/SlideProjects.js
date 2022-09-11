0d7b1d75b74afe2c7b6d44363224e7946bf50f15
Laadjal Faouzi <faouzi.laadjal99@gmail.com>
2022-09-11 22:51:20 +0100

    testing of autoplay

--- src/Components/Projects/SlideProjects.js
+++ src/Components/Projects/SlideProjects.js
@@ -10,13 +10,24 @@ export default function SlideProjects() {
         data-aos-duration="1000"
       >
         <h2 className="heading">Some of the projects that i have done</h2>
-        <Splide aria-label="My Favorite Images">
-          {projects.map((item, index) => (
-            <SplideSlide>
-              <ProjectCard key={index} item={item} />
-            </SplideSlide>
-          ))}
-        </Splide>
+        <div className="flex">
+          <Splide
+            options={{
+              type: "loop",
+              perpage: 1,
+              autoplay: "true",
+              lazyload: "false",
+              speed: "1000",
+            }}
+            aria-label="My Favorite Images"
+          >
+            {projects.map((item, index) => (
+              <SplideSlide>
+                <ProjectCard key={index} item={item} />
+              </SplideSlide>
+            ))}
+          </Splide>
+        </div>
       </div>
     </section>
   );
