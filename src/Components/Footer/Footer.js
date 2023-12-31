e83ea56af120146a21ea576897ab6e75a15cf221
Faouzi Laadjal <faouzi.laadjal99@gmail.com>
2023-12-31 14:37:03 +0100

    Footer updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <StyledFooter className="container__wrapper">
       <div className="container">
-        <span>© 2022 | Laadjal Faouzi |</span>
+        <span>© 2024 | Laadjal Faouzi |</span>
       </div>
     </StyledFooter>
   );
